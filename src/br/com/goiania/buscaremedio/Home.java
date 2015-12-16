24f07874775b612a5df2cad7abc593c1c8d26a5e
juniobc <juniobc@gmail.com>
2015-12-16 11:04:47 -0200

    iniciando o projeto

---
+++ src/br/com/goiania/buscaremedio/Home.java
@@ -0,0 +1,17 @@
+package br.com.goiania.buscaremedio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+	
+	@Override
+	public void onCreate(Bundle bundle){
+		
+		super.onCreate(bundle);
+		
+		
+		
+	}
+
+}
